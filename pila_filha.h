6711852b0bc90ce1474823bec379c6f0a8696f50
Grazielle Tomaz Neves <105025145+grazielleneves@users.noreply.github.com>
2022-10-31 12:18:59 -0300

    Create pila_filha.h

---
+++ pila_filha.h
@@ -0,0 +1,42 @@
+#ifndef PILAFILHA_H
+#define PILAFILHA_H
+#define ERRO_CHEIA -1
+
+// valor do elemento
+typedef int t_chave;
+
+
+// estrutura do "tipo elemento"
+typedef struct t_elemento {
+	t_chave chave;
+} t_elemento;
+
+typedef struct t_no *t_apontador;
+// tipo nó
+typedef struct t_no {
+	t_elemento elemento;
+	t_apontador proximo;
+} t_no;
+
+// tipo conjunto
+typedef struct{
+	t_apontador primeiro;
+	t_apontador ultimo;
+  	t_apontador topo;
+} t_conjunto;
+
+// OPERAÇÕES SUPORTADAS
+// FILA
+void criar_f(t_conjunto *conjunto);
+int enfileirar(t_conjunto *conjunto, t_elemento elemento);
+int desenfileirar(t_conjunto *conjunto,t_elemento elemento);
+int vazia_f(t_conjunto *conjunto);
+void libera_f(t_conjunto *conjunto);
+// PILHA
+void criar_p(t_conjunto *conjunto);
+int empilhar(t_conjunto *conjunto, t_elemento elemento);
+int desempilhar(t_conjunto *conjunto, t_elemento elemento);
+int vazia_p(t_conjunto *conjunto);
+void libera_p(t_conjunto *conjunto);
+
+#endif
